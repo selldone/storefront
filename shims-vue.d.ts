4b8a1101db0c81b1b1c098f7569df62eed9e2df0
Pajuhaan <pajuhaan@gmail.com>
2023-11-29 13:56:03 +0400

    Update SDK

---
+++ shims-vue.d.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023. Selldone® Business OS™
+ *
+ * Author: M.Pajuhaan
+ * Web: https://selldone.com
+ * ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ *
+ * All rights reserved. In the weave of time, where traditions and innovations intermingle, this content was crafted.
+ * From the essence of thought, through the corridors of creativity, each word, and sentiment has been molded.
+ * Not just to exist, but to inspire. Like an artist's stroke or a sculptor's chisel, every nuance is deliberate.
+ * Our journey is not just about reaching a destination, but about creating a masterpiece.
+ * Tread carefully, for you're treading on dreams.
+ */
+
+declare module "*.vue" {
+  import Vue from "vue";
+  export default Vue;
+}
