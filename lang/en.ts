44630851d86c5cf5e41d8b85bf51afd72b9f4498
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2024-05-28 20:19:58 +0400

    Update

--- lang/en.ts
+++ lang/en.ts
@@ -1,12 +1,16 @@
 import global from "@selldone/translate/global/en";
 import icons from "@selldone/translate/global/icons/ltr";
+import countries from "@selldone/translate/global/countries/en";
 import community from "@selldone/translate/global/community/en";
 import pack from "@selldone/translate/shop/en";
-import {en} from "vuetify/locale";
+import { en } from "vuetify/locale";
 
 export default {
   /** ---------------- Icons ---------------- **/
   icons: icons,
+
+  /** ---------------- Countries ---------------- **/
+  countries: countries,
 
   /** ---------------- Global ---------------- **/
   global: global,
@@ -18,5 +22,4 @@ export default {
   ...pack,
 
   $vuetify: en,
-
 };
--- lang/fa.ts
+++ lang/fa.ts
@@ -1,12 +1,16 @@
 import global from "@selldone/translate/global/fa";
 import icons from "@selldone/translate/global/icons/rtl";
+import countries from "@selldone/translate/global/countries/fa";
 import community from "@selldone/translate/global/community/fa";
 import pack from "@selldone/translate/shop/fa";
-import {fa} from "vuetify/locale";
+import { fa } from "vuetify/locale";
 
 export default {
   /** ---------------- Icons ---------------- **/
   icons: icons,
+
+  /** ---------------- Countries ---------------- **/
+  countries: countries,
 
   /** ---------------- Global ---------------- **/
   global: global,
@@ -18,5 +22,4 @@ export default {
   ...pack,
 
   $vuetify: fa,
-
 };
