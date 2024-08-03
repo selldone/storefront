92f71f8d19c6ccd75a2b1caa2f83a75a02b737d2
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2024-08-03 12:41:25 +0400

    Update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
     browser: true, // if your code runs in a browser environment, you should add this
      es2022: true, // 👈 add this
   },
-  extends: ["plugin:vue/vue3-essential", "@vue/prettier", "@vue/typescript"],
+  extends: ["plugin:vue/vue3-essential", "@vue/prettier"],
   rules: {
     "no-console": "off", //process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": "off", //process.env.NODE_ENV === "production" ? "error" : "off"
